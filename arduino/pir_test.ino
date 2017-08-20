82295ddfc914f75e4a1e0be95513b3743bcc031b
Ben Lerchin <ben@lerchin.com>
2017-08-20 00:12:30 -0700

    stl files and arduino tests

---
+++ arduino/pir_test.ino
@@ -0,0 +1,47 @@
+/***********************************************************************
+ * Exp5_Bumpers -- RedBot Experiment 5
+ * 
+ * Now let's experiment with the whisker bumpers. These super-simple switches
+ * let you detect a collision before it really happens- the whisker will
+ * bump something before your robot crashes into it.
+ * 
+ * This sketch was written by SparkFun Electronics, with lots of help from 
+ * the Arduino community.
+ * This code is completely free for any use.
+ * Visit https://learn.sparkfun.com/tutorials/redbot-inventors-kit-guide 
+ * for SIK information.
+ * 
+ * 8 Oct 2013 M. Hord
+ * Revised 30 Oct 2014 B. Huang
+ ***********************************************************************/
+
+#include <RedBot.h>
+
+const int buzzerPin = 9;
+const int ledPin = 13;
+const int pirLeftPin = A0;
+const int pirRightPin = A1;
+
+void setup()
+{
+  pinMode(pirLeftPin, INPUT_PULLUP);
+  pinMode(pirRightPin, INPUT_PULLUP);
+  pinMode(buzzerPin, OUTPUT);  // configures the buzzerPin as an OUTPUT
+  pinMode(ledPin, OUTPUT);
+  delay(2000);
+}
+
+void loop()
+{
+    if(digitalRead(pirRightPin) == LOW) {
+      digitalWrite(ledPin, HIGH);
+    } else if (digitalRead(pirLeftPin) == LOW) {
+      digitalWrite(ledPin, HIGH);
+    } else {
+      digitalWrite(ledPin, LOW);
+      noTone(buzzerPin);
+    }
+    //  the variable "buzzerPin". 
+    delay(125);   // Wait for 125ms. 
+}
+
